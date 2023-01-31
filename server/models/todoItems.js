606307370e6230b85b3f2f1f3744f1224dce0298
PetraCrmaric <petracrm@hotmail.com>
2023-01-31 12:31:30 +0100

    first commit

---
+++ server/models/todoItems.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const TodoItemsSchema = new mongoose.Schema({
+    item:{
+        type: String,
+        required: true
+    }
+})
+ 
+module.exports = mongoose.model('todo', TodoItemsSchema);
